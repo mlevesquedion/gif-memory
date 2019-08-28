d0e26f9538de27b3012d841833e0f4824327dd9e
Michaël Lévesque-Dion <mlevesquedion@gmail.com>
2019-08-27 20:43:13 -0400

    Update todos

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,7 @@ import 'package:gif_memory/widgets/home_page.dart';
 
 import 'constants.dart';
 
-// TODO: verify card_collection tests
-// TODO: fix bugs (card flipping)
 // TODO: test widgets
-// TODO: make it look good
 // TODO: "preload" images so that they don't lag on first render
 // TODO: (maybe) show cards at the start
 
